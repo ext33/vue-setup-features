711b26423f035597a875d577c0272bd6e1c330e0
ext33 <redwoodx3@gmail.com>
2021-01-14 02:41:56 +0300

    fix global-setup.sh

--- django setup/global-setup.sh
+++ django setup/global-setup.sh
@@ -1,42 +1,42 @@
 clear
-if [-n "$1"]
+echo
+
+if [ -n "$1" ]
 then
 case "$1" in
 --full) python3 -m pip install django djangorestframework pillow selenium
-echo "" 
+echo 
 echo "All packeges installed, enjoy!";;
 --min) python3 -m pip install django 
-echo "" 
+echo
 echo "Only django installed, enjoy!";;
 *) echo "$1 is not an option" ;;
 esac
 else
+
 python3 -m pip install django
 
-echo ""
-echo -n "Do you want to install django-rest-framework? (y/n): "
-read rest
-if [$rest = 'y']
+echo
+read -p "Do you want to install django-rest-framework? (y/n): " rest
+if [ "$rest" = "y" ]
 then
 python3 -m pip install djangorestframework
 fi
 
-echo ""
-echo -n "Do you want to install pillow? (y/n): "
-read pillow
-if [$rest = 'y']
+echo
+read -p "Do you want to install pillow? (y/n): " pillow
+if [ "$pillow" = "y" ]
 then
 python3 -m pip install pillow
 fi
 
-echo ""
-echo -n "Do you want to install selenium? (y/n): "
-read rest
-if [$rest = 'y']
+echo
+read -p "Do you want to install selenium? (y/n): " selenium
+if [ "$selenium" = "y" ]
 then
 python3 -m pip install selenium
 fi
 
-echo "" 
-echo "Selected packeges installed, enjoy!";;
+echo
+echo "Selected packeges installed, enjoy!"
 fi
